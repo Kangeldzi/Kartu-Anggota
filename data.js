623520237ce3e386c78d9bf0486c30f382ab5cd8
Kang L <143698732+Kangeldzi@users.noreply.github.com>
2026-01-29 01:32:02 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+// data.js
+const MEMBERS_DATA = [
+  {
+    "ID": "KOP-001",
+    "Nama": "Kang L",
+    "Email": "siliwangi@email.com",
+    "Status": "AKTIF",
+    "Posisi": "Ketua",
+    "Alamat": "Pamijahan, Bogor",
+    "WhatsApp": "085218483129",
+    "Jenis Kelamin": "Laki-laki",
+    "Agama": "Islam",
+    "NIK": "3201171212810011",
+    "Tanggal Bergabung": "18-09-2025",
+    "Simpanan Pokok": 200000,
+    "Simpanan Wajib": 50000,
+    "Simpanan Sukarela": 100000,
+    "Total Pinjaman": 0,
+    "SHU Estimasi": 150000,
+    "Foto": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhADAiDm28b0fescUKnqADc9uk3Q2oIptWD-hyzBQXb2AMtoPrxb7Q9Ecr9SDV_RhqA9uWO6qTcepfbfuR7KFWuKUmYYPB-6JfiExBCzT-MljhNh1P1TPsabKvh2rAQzEtZUA0lpiIkIuBxHvHVuXl0GdUWEyaABcfpsHnX_kyDLN_pS2gVNzp_kIP6Vss/s1280/1000007204.jpg"
+  },
+  // Tambahkan data lainnya
+];
