03428316a63f0e1fe097cb5f37df1768f5c74512
Kang L <143698732+Kangeldzi@users.noreply.github.com>
2026-01-29 02:04:02 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+// data.js
+const MEMBERS_DATA = [
+  {
+    "ID": "KOP-001",
+    "Nama": "Kang L",
+    "Email": "test@example.com",
+    "Status": "AKTIF",
+    "Alamat": "Bogor",
+    "WhatsApp": "08123456789",
+    "Jenis Kelamin": "Laki-laki",
+    "Agama": "Islam",
+    "NIK": "1234567890123456",
+    "Tanggal Bergabung": "2024-01-01",
+    "Simpanan Pokok": 100000,
+    "Simpanan Wajib": 50000,
+    "Simpanan Sukarela": 0,
+    "Total Pinjaman": 0,
+    "SHU Estimasi": 0,
+    "Foto": ""
+  }
+];
